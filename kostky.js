a69518be79ad0457f244d54c81d8cf97ed5b6b3e
jachymneumiprogramovat <82580668+jachymneumiprogramovat@users.noreply.github.com>
2021-11-03 19:27:58 +0100

    Now you can create your own custom dices and test the probability for them. Also there is a fairly accurate tip on how it will end up.

--- kostky.js
+++ kostky.js
@@ -1,11 +1,20 @@
 var ans = []
 let obj = {}
-for(let i5=1;i5<=6;i5++){
-    for(let iiii=1;iiii<=6;iiii++){
-        for(let iii=1;iii<=6;iii++){
-            for(let i=1;i<=6;i++){
-                for(let ii=1;ii<=6;ii++){
-                    let sum = i+ii+iii+iiii+i5
+
+//calculating the gues of the outcome
+let dices = 5;
+let values = [1,2,3,4,5,6]
+let avg = values.reduce((sum, curr)=>{return sum+curr})/values.length
+let exp = avg*dices
+console.log(exp, avg)
+
+//trying every posible combination
+for(let i5=0;i5<=values.length-1;i5++){
+    for(let iiii=0;iiii<=values.length-1;iiii++){
+        for(let iii=0;iii<=values.length-1;iii++){
+            for(let i=0;i<=values.length-1;i++){
+                for(let ii=0;ii<=values.length-1;ii++){
+                    let sum = values[i]+values[ii]+values[iii]+values[iiii]+values[i5]
                     if(!ans.includes(sum)){
                         obj[sum]=0
 
@@ -22,4 +31,3 @@ for(let i5=1;i5<=6;i5++){
     }
 }
 console.log(obj)
-//if you are not dumb as i am you can just multiply the number of dices by 3,5 and you will get the result. If it is in the middle of two numberes then it is both of them.
