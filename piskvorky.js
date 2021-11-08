4b03d7c4f7e459f6010c0911a7445f52ec52e467
jachymneumiprogramovat <82580668+jachymneumiprogramovat@users.noreply.github.com>
2021-11-08 09:44:11 +0100

    lets go

--- piskvorky.js
+++ piskvorky.js
@@ -1,12 +1,17 @@
     //variabli co si ukradnu z DOMu
     let okynka = document.getElementsByClassName("jjh");
+    let rady = document.getElementsByClassName("rady")
+
+//tady si beru tu tabulku na statistiky
+    let tabulka = document.getElementById("tabulka")
     let KdoHraje = document.getElementById("kdohraje");
     let PocetKol = document.getElementById("kolo")
     let Pocitadlo = document.getElementById("vyhry")
 
-    let tabulka = document.getElementById("tabulka")
+//Tady vytvářím tu tabulku na hru
 
     //další variably
+    let hraciPlocha = 4
     let PocetHer = 0;
     let zmizeni = 0;
     let txt; 
@@ -17,18 +22,23 @@
     //kolik kterýmu políčku zbývá času na hrací ploše
     let mizeni = [];
    //tohle slouží k tomu abych zaznamenábal pozice v tabulce do arraye a mohl je potom čeknout kdo vyhrál 
-    let Arr = [
-        ["","","",""],
-        ["","","",""],
-        ["","","",""],
-        ["","","",""],
-    ]
+    let Arr = new Array(hraciPlocha+1)
+for (var i = 0; i < Arr.length; i++) {
+  Arr[i] = new Array(hraciPlocha+1);
+}
+for(let i of Arr){  
+    for(let j=0;j<hraciPlocha+1;j++){
+        i[j]=" "
+    }
+}
+
+
 let vyhry = [0,0];
 
 
 //plnění mizecího arry
     for(let i of okynka){
-mizeni[i]=0;
+mizeni[i]=NaN ;
     }
 
 class hra{
@@ -40,13 +50,17 @@ class hra{
     } 
     play(){
         //tohle je defakto celá hra...
-        okynka[this.poradi].innerHTML=hrac[kolo%2];
+
+        rady[this.tr].getElementsByClassName("jjh")[this.td].innerHTML=hrac[kolo%2]
         Arr[this.tr][this.td]=hrac[kolo%2];
+
+        console.log(this.td,this.tr, this.poradi,hraciPlocha, Arr)
         kolo++;
+  
     }
     check(){
         //haloo haloo je tam někdo?
-        if (okynka[this.poradi].innerHTML!=""){
+        if (okynka[this.poradi].innerHTML=="X" || okynka[this.poradi].innerHTML=="O"){
         console.log("tak jsi úplněj retard? a podvádět se nemá...");
         kolo++;
             if(kolo%2==0){
@@ -69,7 +83,7 @@ class hra{
     odeber(){
         //tady nastavím kolik kol to vydrží
         mizeni[this.poradi]=8;
-        for(let i = 0;i<okynka.length;i++){
+        for(let i = 0;i<mizeni.length;i++){
             //tady se mění ta barva podle toho jak dlouho tam bude
             switch(mizeni[i]){
                 case 8:okynka[i].style.color="black";
@@ -115,11 +129,19 @@ class hra{
                     okynka[i].innerHTML=" ";
                     mizeni[i]=NaN;
                     //tady se podle toho celkového pořadí vypočítá kde to je v tom array a tam se to odebere,
-                    Arr[Math.floor(i/4)][i%4]="";
+                    Arr[Math.floor(i/(hraciPlocha+1))][i%(hraciPlocha+1)]="";
                     zmizeni++;
                 break;
+                
+                case NaN: 
+                okynka[i].innerHTML=" ";
+                Arr[Math.floor(i/(hraciPlocha+1))][i%(hraciPlocha+1)]="";
+                    break;
             }
+        //okynka[i].style.padding=`${100/(hraciPlocha/2)}px`
+        //okynka[i].style.fontSize = `${50/(hraciPlocha/3)}px`
         }
+
     }
     pocitadla(){
         //tady jsou jen takový ty zbytečnosti ale přibidou i super věěci
@@ -151,34 +173,9 @@ class hra{
         }
     }
     checkTheWinner(){
-        let cP = (kolo-1)%2
-       //řady
-       for (let i = 0;i<4;i++){
-           if(Arr[i][0]==Arr[i][1]&&Arr[i][2]==Arr[i][3]&&Arr[i][0]!=""&&Arr[i][3]!==""){
-            konec=true;
-            vyhry[cP]++;
-            }
-        }
-       //ta druhá věc
-       for (let j = 0;j<4;j++){
-        if(Arr[0][j]==Arr[1][j]&&Arr[2][j]==Arr[3][j]&&Arr[0][j]!=""&&Arr[3][j]!=""){
-            konec=true;
-            vyhry[cP]++;
-            }
-        }
-
-
-        //diagonály
-        if(Arr[0][0]==Arr[1][1]&&Arr[2][2]==Arr[3][3]&&Arr[0][0]!=""&&Arr[3][3]!=""){
-            konec=true;
-            vyhry[cP]++;
-            }
-        if(Arr[0][3]==Arr[1][2]&&Arr[2][1]==Arr[3][0]&&Arr[0][3]!=""&&Arr[3][0]!=""){
-            konec=true;
-            vyhry[cP]++;
-            }
-        }
-    clear(){
+        //tahle funkce bude chtít hodně předělat, ale myslím, že by to mělo jít. budu schopnej se vždycky podívat na čtyři na každou stranu a nenarazím na konec protože, to ještě předtím než se na to podívám rozšířím tak aby to sedělo.
+    }
+        clear(){
         for(let el=0; el<okynka.length;el++){
         //čištění hrací plochy
            okynka[el].innerHTML=" ";
@@ -188,47 +185,95 @@ class hra{
         kolo=0;
         }
         //čištění pozic v tom kdo vyhrál
-        Arr = [
-        ["","","",""],
-        ["","","",""],
-        ["","","",""],
-        ["","","",""],
-        ]
+        for(let i of Arr){  
+            for(let j=0;j<hraciPlocha+1;j++){
+                i[j]=" "
+            }
+        }
         //čištění toho kolikrát jsi podváděl
         hrac = ["O","X",0,0];
         konec=false;
         zmizeni = 0
     }
+    posunuti(){
+        if(this.td<2 || (hraciPlocha-this.td)<2){
+            let zbytek = 1
+            //tady se přidává novej sloupec
+            for(let i=0;i<zbytek;i++){
+                let rr = document.createElement("tr")
+                tt.appendChild(rr)
+                rr.id=(hraciPlocha+1).toString()
+                rr.className="rady"
+
+                //tady se ten nový řádek plní
+                for(let j=0;j<=hraciPlocha;j++){
+                    let sloupec = document.createElement("td")
+                    rr.appendChild(sloupec)
+                    sloupec.className="jjh"
+                    sloupec.innerHTML=rady.length;
+                    
+                }
+            }
+            //tady se přidávají nové
+            for(let pp of rady){
+                let sloupec = document.createElement("td")
+                sloupec.className="jjh"
+                pp.appendChild(sloupec)
+            }
+
+            //tady se zvětšuje ten array o jedno míst (ten velkej)
+            for(let j=0;j<zbytek;j++){
+                Arr.push(new Array())
+
+                for(let k=0;k<=hraciPlocha;k++){
+                    Arr[Arr.length-1].push(" ")
+                }
+            }
+
+            for(let i of Arr){
+                    i.push(" ")
+            }
+
+           hraciPlocha++
+        }
+           
+        if(this.tr<2 || (hraciPlocha-this.tr)<2){
+            //console.log('prdel')
+        }
+    }
 }
     
 
+let tt = document.getElementById("tt")
 
-    let tt = document.getElementById("tt")
-    for(let i=0;i<4;i++){
+    for(let i=0;i<=hraciPlocha;i++){
         i=i.toString()
         let rr = document.createElement("tr")
+
         tt.appendChild(rr)
         rr.id= i.toString()
+        rr.className="rady"
 
-        for(let j=0;j<4;j++){
+        for(let j=0;j<=hraciPlocha;j++){
             j=j.toString()
             let sloupec = document.createElement("td")
             rr.appendChild(sloupec)
-            sloupec.className="jjh"
-            
-            let poradi = i*3+parseInt(j)+parseInt(i)
 
+            sloupec.className="jjh"
+            sloupec.innerHTML=" ";
+
+            let poradi = i*hraciPlocha+parseInt(j)+parseInt(i)
             sloupec.onclick =     function hraj (){
-                let hraj = new hra(i,j,poradi);
-                console.log(i,j,poradi)
+                let hraj = new hra(i,j,poradi); 
                 if (hraj.check()==true){
+                    hraj.posunuti()
                     hraj.play();
                 };
             
                 //tady se počítá kolik má jaká kostička ještě zůstat na herním pláně
                 hraj.odeber();
                 //kdo vyhrál? zatím docela amatérská metoda koukání se na to kdo vyhrál to by chtělo zlepšit
-                hraj.checkTheWinner();
+                //hraj.checkTheWinner();
                 if(konec==true){
                     //tady se přidá novej záznam do tabulky
                     hraj.tabulka()
